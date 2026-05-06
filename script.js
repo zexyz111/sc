7910fa45e71eecfbd02db6f373af4e552d5fe8ed
zexyz111 <vl.06.litvinenko@gmail.com>
2026-05-06 21:42:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const canvas = document.getElementById('snow');
 const ctx = canvas.getContext('2d');
-let w, h, snowflakes = [];
+let w, h, flakes = [];
 
 function init() {
     w = window.innerWidth;
     h = window.innerHeight;
-    canvas.width = w; canvas.height = h;
-    snowflakes = [];
-    for(let i = 0; i < 150; i++) {
-        snowflakes.push({
+    canvas.width = w;
+    canvas.height = h;
+    flakes = [];
+    for(let i = 0; i < 120; i++) {
+        flakes.push({
             x: Math.random() * w,
             y: Math.random() * h,
             r: Math.random() * 2 + 0.5,
-            v: Math.random() * 1 + 0.5
+            v: Math.random() * 0.8 + 0.3
         });
     }
 }
@@ -21,7 +22,7 @@ function draw() {
     ctx.clearRect(0, 0, w, h);
     ctx.fillStyle = "rgba(255, 255, 255, 0.4)";
     ctx.beginPath();
-    for(let f of snowflakes) {
+    for(let f of flakes) {
         ctx.moveTo(f.x, f.y);
         ctx.arc(f.x, f.y, f.r, 0, Math.PI*2);
         f.y += f.v;
